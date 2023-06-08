276f5bac740b7fb27d6c7376ec5d9bc016bad280
Fabien Dev <fabiendevfront@gmail.com>
2023-06-08 12:13:12 +0200

    Start create portfolio page, add data projects in works.json, get data of works with hook useFetch

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,8 +1,28 @@
+import { useFetch } from "../services/useFetch.jsx";
+
 const Portfolio = () => {
+    const { data, loading, error } = useFetch("/data/works.json");
+    console.log(data);
+
     return (
-        <div>
-            <h1>Portfolio</h1>
-        </div>
+        <>
+            {loading ? (
+                <span>Chargement des données...</span>
+            ) : error && !loading ? (
+                <span>Erreur lors du chargement des données</span>
+            ) : data ? (
+                <section className="portfolio">
+                    <div className="portfolio__head">
+                        <div className="portfolio__container">
+                            <h2 className="portfolio__title">Mes réalisations</h2>
+                            <p className="portfolio__subtitle">Vous retrouverez ci-dessous mes réalisations en développement web</p>
+                        </div>
+                    </div>
+
+                </section>) : (
+                <span>La page rencontre un problème</span>
+            )}
+        </>
     );
 };
 
